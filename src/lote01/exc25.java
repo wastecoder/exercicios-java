9d52169a849bb4d5f5dee98715a1d466d1b78f89
Paulino <paulinomendes03@gmail.com>
2023-02-17 00:43:58 -0300

    refactor(exc25): transforma o exercício em método

--- src/lote01/exc25.java
+++ src/lote01/exc25.java
@@ -1,36 +1,36 @@
 package lote01;
 
 public class exc25 {
-public static void main(String[] args) {
-    int minutoTotal, totalInicio, totalFinal, horaTotal;
+    public static void main(String[] args) {
+        prazo24h(22, 8, 15, 34);
+        prazo24h(10, 0, 10, 55);
+        prazo24h(10, 23, 15, 46);
+    }
 
-        //ENTRADA DE DADOS DO USU√ÅRIO
-    int horaInicio = 22, minutoInicio = 8, horaFim = 15, minutoFim = 34; //17h26min
-//    int horaInicio = 10, minutoInicio = 0, horaFim = 10, minutoFim = 55; //24h55min
-//    int horaInicio = 10, minutoInicio = 23, horaFim = 15, minutoFim = 46; //5h23min
+    public static void prazo24h(int horaInicio, int minutoInicio, int horaFim, int minutoFim) {
+        int minutoTotal, totalInicio, totalFinal, horaTotal;
 
-    totalInicio = (60*horaInicio) + minutoInicio;
-    totalFinal = (60*horaFim) + minutoFim;
+        totalInicio = (60*horaInicio) + minutoInicio;
+        totalFinal = (60*horaFim) + minutoFim;
 
-    if (horaInicio < horaFim) { //Terminou no mesmo dia
-        minutoTotal = (totalFinal - totalInicio);
-        horaTotal = minutoTotal/60;
-//        System.out.println("Comecou = " + horaInicio + ":" + minutoInicio + "; Terminou = " + horaFim + ":" + minutoFim);
-//        System.out.println("Minutos = " + minutoTotal + "; Horas = " + (float)minutoTotal/60);
-        System.out.println(">>> Tempo total = " + horaTotal + "h" + (minutoTotal%60) +"min");
-        System.out.println(">>> Terminou dentro do prazo de 24h!");
-    } else { //Terminou em outro dia
-        minutoTotal = (1440 - totalInicio) + totalFinal;
-        horaTotal = minutoTotal/60;
-//        System.out.println("Comecou = " + horaFim + ":" + minutoFim + "; Terminou = " + horaInicio + ":" + minutoInicio);
-//        System.out.println("Minutos = " + minutoTotal + "; Horas = " + minutoTotal/60);
-        System.out.println(">>> Tempo total = " + horaTotal + "h" + (minutoTotal%60) +"min");
+        System.out.println(">>> Hora de inÌcio = " + horaInicio + ":" + minutoInicio);
+        System.out.println(">>> Hora do final = " + horaFim + ":" + minutoFim);
 
-        if (minutoTotal < 1440) {
-            System.out.println(">>> Terminou dentro do prazo de 24h!");
-        } else {
-            System.out.println(">>> Ultrapassou o prazo de 24h!");
+        if (horaInicio < horaFim) { //Terminou no mesmo dia
+            minutoTotal = (totalFinal - totalInicio);
+            horaTotal = minutoTotal/60;
+            System.out.println(">>> Tempo total = " + horaTotal + "h" + (minutoTotal%60) +"min");
+            System.out.println(">>> Terminou dentro do prazo de 24h!\n");
+        } else { //Terminou em outro dia
+            minutoTotal = (1440 - totalInicio) + totalFinal;
+            horaTotal = minutoTotal/60;
+            System.out.println(">>> Tempo total = " + horaTotal + "h" + (minutoTotal%60) +"min");
+
+            if (minutoTotal < 1440) {
+                System.out.println(">>> Terminou dentro do prazo de 24h!\n");
+            } else {
+                System.out.println(">>> Ultrapassou o prazo de 24h!\n");
+            }
         }
     }
-}
 }
