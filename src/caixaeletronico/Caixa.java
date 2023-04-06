38937ab4c85b5e6246339a90dffda60380b2f886
Paulino <paulinomendes03@gmail.com>
2023-04-06 15:49:10 -0300

    refactor(Caixa): melhora o código em geral

--- src/caixaeletronico/Caixa.java
+++ src/caixaeletronico/Caixa.java
@@ -6,11 +6,7 @@ public class Caixa {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Queue<Integer> notasCaixa = new PriorityQueue<>(6);
-
-        menu(input, notasCaixa);
-    }
-
-    public static void menu(Scanner input, Queue<Integer> notasCaixa) {
+        List<Integer> notasValidas = new ArrayList<>(List.of(2, 5, 10, 20, 50, 100, 200));
         int[][] bancos = new int[4][5];
         int escolha;
 
@@ -29,28 +25,25 @@ public class Caixa {
 
             switch (escolha) {
                 case 0 -> System.out.println("\n>> Programa finalizado.");
-                case 1 -> carregarNotas(input, notasCaixa);
+                case 1 -> carregarNotas(input, notasCaixa, notasValidas);
                 case 2 -> sacarPorValor(input, notasCaixa, bancos);
-                case 3 -> sacarPorValorENotas(input, notasCaixa, bancos);
+                case 3 -> sacarPorValorENotas(input, notasCaixa, bancos, notasValidas);
                 case 4 -> exibirEstatisticas(bancos);
                 default -> System.out.println(">> Escolha inválida, tente novamente.");
             }
         } while (escolha != 0);
     }
 
-
-
-    public static void carregarNotas(Scanner input, Queue<Integer> notasCaixa) {
+    public static void carregarNotas(Scanner input, Queue<Integer> notasCaixa, List<Integer> notasValidas) {
         if (!notasCaixa.isEmpty()) {
             System.out.println(">> Caixa já possui dinheiro.");
             System.out.print("> Deseja sobrescrever o saldo atual? [S/N]: ");
             String resposta = input.next().toUpperCase(Locale.ROOT);
 
-            if (!resposta.equals("S")) menu(input, notasCaixa);
+            if (!resposta.equals("S")) return;
         }
 
         notasCaixa.clear();
-        List<Integer> notasValidas = new ArrayList<>(List.of(2, 5, 10, 20, 50, 100, 200));
         int QUANTIDADE_POR_NOTA = 6;
 
         for (int notasValida : notasValidas) {
@@ -73,9 +66,9 @@ public class Caixa {
 
 
     public static void sacarPorValor(Scanner input, Queue<Integer> notasCaixa, int[][] bancos) {
-        if (notasCaixa.isEmpty()) { //TRANSFORMAR EM MÉTODO
+        if (notasCaixa.isEmpty()) { //O ideal seria reutilizar com método, mas não dá pra fazer isso com o return
             System.out.println(">> Caixa vazio. Carregue notas antes.");
-            menu(input, notasCaixa);
+            return;
         }
 
         int saldoCaixa = saldoCaixa(notasCaixa);
@@ -90,11 +83,7 @@ public class Caixa {
         } else {
             coletarEstatisticas(bancos, coletarBanco(input), valorDesejado);
 
-            while (valorDesejado % 5 != 0 && notasCaixa.contains(2)) { //Retira números não múltiplos de 5 até virar múltiplo dele.
-                System.out.println("RETIRANDO >>>> 2");
-                valorDesejado -= 2;
-                notasCaixa.remove(2);
-            }
+            valorDesejado = transformarEmMultiploDe5(notasCaixa, valorDesejado);
 
             List<Integer> notasValidas = new ArrayList<>(List.of(200, 100, 50, 20, 10, 5, 2));
             for (int notaAtual : notasValidas) {
@@ -104,15 +93,13 @@ public class Caixa {
         }
     }
 
-    public static void sacarPorValorENotas(Scanner input, Queue<Integer> notasCaixa, int[][] bancos) {
-        if (notasCaixa.isEmpty()) {
+    public static void sacarPorValorENotas(Scanner input, Queue<Integer> notasCaixa, int[][] bancos, List<Integer> notasValidas) {
+        if (notasCaixa.isEmpty()) { //O ideal seria reutilizar com método, mas não dá pra fazer isso com o return
             System.out.println(">> Caixa vazio. Carregue notas antes.");
-            menu(input, notasCaixa);
+            return;
         }
 
-        ArrayList<Integer> notasValidas = new ArrayList<>(List.of(200, 100, 50, 20, 10, 5, 2));
         int saldoCaixa = saldoCaixa(notasCaixa);
-
         System.out.println(">> Saldo do caixa: R$" + saldoCaixa);
         System.out.println(">> Notas válidas: " + notasValidas);
 
@@ -128,12 +115,6 @@ public class Caixa {
             int valorDesejado = notaDesejada * quantidadeDesejada;
             int valorAuxiliar = valorDesejado;
             int codigoBanco = coletarBanco(input);
-
-            while (valorDesejado % 5 != 0 && notasCaixa.contains(2)) { //TRANSFORMAR EM MÉTODO
-                System.out.println("RETIRANDO >>>> 2");
-                valorDesejado -= 2;
-                notasCaixa.remove(2);
-            }
 
             for (int i = notasValidas.size() - 1; i >= 0; i--) { //Remove as notas maiores que a desejada. Auxilia a repetição abaixo
                 if (notasValidas.get(i) <= notaDesejada) continue;
@@ -158,6 +139,16 @@ public class Caixa {
         return valorDesejado;
     }
 
+    public static int transformarEmMultiploDe5(Queue<Integer> notasCaixa, int valorDesejado) {
+        //Retira notas de 2 até virar múltiplo de 5.
+        while (valorDesejado % 5 != 0 && notasCaixa.contains(2)) {
+            System.out.println("RETIRANDO >>>> 2");
+            valorDesejado -= 2;
+            notasCaixa.remove(2);
+        }
+
+        return valorDesejado;
+    }
 
 
     public static int coletarBanco(Scanner input) {
